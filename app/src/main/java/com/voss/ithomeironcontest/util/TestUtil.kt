604a011df3e73b7a2f65101155287f70da762b5d
VossCG <84518506+VossCG@users.noreply.github.com>
2023-09-16 20:45:17 +0800

    Delete app directory

--- app/src/main/java/com/voss/ithomeironcontest/util/TestUtil.kt
+++
@@ -1,6 +0,0 @@
-package com.voss.ithomeironcontest.util
-
-import android.content.Context
-import android.widget.Toast
-
-fun showToast(context: Context,message:String):Unit = Toast.makeText(context,message,Toast.LENGTH_SHORT).show()
