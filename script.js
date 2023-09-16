c569433b99641f6cdfceef184ec09a05d0a2af53
VossCG <84518506+VossCG@users.noreply.github.com>
2023-09-16 20:47:53 +0800

    web src

---
+++ script.js
@@ -0,0 +1,21 @@
+let displayValue = "";
+
+function appendToDisplay(value) {
+    displayValue += value;
+    document.getElementById("display").value = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = "";
+    document.getElementById("display").value = displayValue;
+}
+
+function calculateResult() {
+    try {
+        displayValue = eval(displayValue);
+        document.getElementById("display").value = displayValue;
+    } catch (error) {
+        displayValue = "錯誤";
+        document.getElementById("display").value = displayValue;
+    }
+}
